700fd9e53b1eda113d51bddaaa73ac8a605f554d
kevindcp <kevindchaconp@gmail.com>
2021-10-25 19:55:53 -0400

    fixed issues

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -25,7 +25,6 @@ import Category from 'src/category/category.entity';
           process.env.NODE_ENV === 'production'
             ? { rejectUnauthorized: false }
             : false,
-        }
       })
     }),
   ],
