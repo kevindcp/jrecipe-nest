25f6bdbb9b6b258d54b553e56588506edfba5eee
kevindcp <kevindchaconp@gmail.com>
2021-10-26 00:59:24 -0400

    api route

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe, ClassSerializerInterceptor}  from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(
     app.get(Reflector))
